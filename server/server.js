bc5edf4934fc8b0b094fb610bcd31a38ad22c50d
zh-rifat <zhrifat.00@gmail.com>
2023-04-27 01:48:35 +0600

    v0.1

--- server/server.js
+++ server/server.js
@@ -38,6 +38,6 @@ app.get("/", (req, res) => {
   res.send("hello");
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT||3000, () => {
   console.log(`app is running on port = ${process.env.PORT}`.bgWhite.black);
 });
